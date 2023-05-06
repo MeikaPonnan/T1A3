963114f37015f07fb3ef75deefdb2d74856ceb4b
MeikaPonnan <meika.ponnan@gmail.com>
2023-05-06 23:13:11 +1000

    Added functions to the list

---
+++ functions.py
@@ -0,0 +1,9 @@
+def add_stock():
+    print("Add ticker")
+
+def remove_stock():
+    print("Remove stock")
+
+def view_stock():
+    print("View stock list")
+
--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+from functions import add_stock, remove_stock, view_stock
+
 
 print("Welcome to your stock tracker")
 
+file_name = "list.csv"
+
 def create_list():
-    print("1. Enter 'add' to add a new stock to your list")
-    print("2. Enter 'remove' to take out a stock from your list")
-    print("3. Enter 'view' to view your stock list")
-    print("4. Enter 'quit' to exit")
+    print("1. Enter '1' to add a new stock to your list")
+    print("2. Enter '2' to take out a stock from your list")
+    print("3. Enter '3' to view your stock list")
+    print("4. Enter '4' to exit")
     choice = input("What do you want to do? ")
     return choice
 
 
+user_input = ""
 
+while user_input != "4":
+    user_input = create_list()
+
+    if (user_input == "1"):
+        add_stock()
+    elif (user_input == "2"):
+        remove_stock()
+    elif (user_input == "3"):
+        view_stock()
+    elif (user_input == "4"):
+        continue
+    else:
+        print("Invalid. Please choose from the list")
+
+    input("Press Enter to continue.")
+
+
+print("Thanks for your time!")
